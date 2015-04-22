43878ea8573719b6532d3128f9f6669c9a9c6123
stelthpon <ponsan.dq.special.player0501@gmail.com>
2015-04-23 07:25:49 +0900

    next_permutation.py 追加
    next_permutationを追加した

---
+++ python/next_permutation/next_permutation.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Owner
+#
+# Created:     23/04/2015
+# Copyright:   (c) Owner 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import itertools
+
+for x in itertools.permutations("abcd"):
+    print (x)
